6cdfe1da4ca4de5ed6f91173d7967a47888ecf18
Derek Hearst <derekallenhearst@gmail.com>
2022-11-08 09:18:35 -0700

    created morning challenge

---
+++ Week3/d2MorningChallenge/app.js
@@ -0,0 +1,9 @@
+function doSubmit() {
+
+	let words = window.event.target.textarea.value.split(" ")
+	maxLength = 0
+	words.forEach(element => {
+		if (element.length > maxLength) { maxLength = element.length }
+	});
+	window.alert(maxLength)
+}
