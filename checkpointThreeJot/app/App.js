472e413b50e02cac09932d22cd19676ec3039655
Derek Hearst <derekallenhearst@gmail.com>
2022-11-11 21:04:11 -0700

    takes out checkpoint

--- checkpointThreeJot/app/App.js
+++
@@ -1,9 +0,0 @@
-import { NotesController } from "./Controllers/NotesController.js";
-import { UsersController } from "./Controllers/UsersController.js";
-
-class App {
-  usersController = new UsersController()
-  notesController = new NotesController()
-}
-
-window["app"] = new App();
