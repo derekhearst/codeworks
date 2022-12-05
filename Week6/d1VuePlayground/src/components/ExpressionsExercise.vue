bb55857b1b522b1c2b882bc95a893d440fee6f21
Derek Hearst <derekallenhearst@gmail.com>
2022-12-05 14:40:22 -0700

    finished

---
+++ Week6/d1VuePlayground/src/components/ExpressionsExercise.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="expression-exercises container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Exercises</h1>
+				<div class="border p-1">
+					<p>Write a math equation Vue can evaluate, inside of the curly braces below:</p>
+
+					<p>{{ 1 + 5 }}</p>
+				</div>
+				<div class="border p-1">
+					<p>Use interpolation to get the customary Hello, World! to display on the screen. Don't forget your quotes</p>
+					<p>{{ "Hello world!" }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
