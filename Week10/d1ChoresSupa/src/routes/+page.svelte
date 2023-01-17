8a3811fa44f7117850276c444f17776a8d9242c1
Derek Hearst <derekallenhearst@gmail.com>
2023-01-17 09:06:00 -0700

    finished supabae intergration

---
+++ Week10/d1ChoresSupa/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Account from '$lib/Account.svelte';
+	import Auth from '$lib/Login.svelte';
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
+
+	let chores = supabase.from('chores').select('*');
+
+	async function createChore() {}
+</script>
+
+<svelte:head>
+	<title>Supabase + SvelteKit</title>
+	<meta name="description" content="SvelteKit using supabase-js v2" />
+</svelte:head>
+
+<form on:submit={createChore} />
+
+<h1>Chores</h1>
+{#if chores.data}
+	{#each chores.data as chore}
+		<div>{chore.name}</div>
+	{/each}
+{/if}
